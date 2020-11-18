78d279142ea985ad270e22f7c329136b53b6f7fc
ZBejavu <67857471+ZBejavu@users.noreply.github.com>
2020-11-18 21:54:54 +0200

    test

--- songclient/src/services/network.js
+++ songclient/src/services/network.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
 
-console.log(process.env.REACT_APP_HOST, "HOOOOOOST")
 console.log(process.env.NODE_ENV , "environment")
 const network = axios.create({
   baseURL: process.env.NODE_ENV === 'production'? process.env.REACT_APP_HOST : 'http://127.0.0.1:5000'
