ebf0d29d82e65fdfc5e07a9f517815428e87b7f5
ZBejavu <67857471+ZBejavu@users.noreply.github.com>
2020-11-21 13:18:08 +0200

    test

--- mysqlscript.sh
+++ mysqlscript.sh
@@ -2,34 +2,23 @@
 
 sudo apt -y install expect
 
-# spawn sudo mysql_secure_installation
-# expect \"Enter current password for root (enter for none):\"
-# send \"$PWD\r\"
-# expect \"Would you like to setup VALIDATE PASSWORD plugin?\"
-# send \"n\r\" 
-# expect \"Change the password for root ?\"
-# send \"n\r\"
-# expect \"Remove anonymous users?\"
-# send \"y\r\"
-# expect \"Disallow root login remotely?\"
-# send \"y\r\"
-# expect \"Remove test database and access to it?\"
-# send \"y\r\"
-# expect \"Reload privilege tables now?\"
-# send \"y\r\"
-
 SECURE_MYSQL=$(expect -c "
 set timeout 10
-spawn mysql -u root #
-expect \"MariaDB\"
-send \"USE mysql;\r\"
-expect \"MariaDB\"
-send \"UPDATE user SET plugin='mysql_native_password' WHERE User='root';\r\"
-expect \"MariaDB\"
-send \"FLUSH PRIVILEGES;\r\"
-expect \"MariaDB\"
-send \"exit;\r\"
-expect eof
+spawn sudo mysql_secure_installation
+expect \"Enter current password for root (enter for none):\"
+send \"$PWD\r\"
+expect \"Would you like to setup VALIDATE PASSWORD plugin?\"
+send \"n\r\" 
+expect \"Change the password for root ?\"
+send \"n\r\"
+expect \"Remove anonymous users?\"
+send \"y\r\"
+expect \"Disallow root login remotely?\"
+send \"y\r\"
+expect \"Remove test database and access to it?\"
+send \"y\r\"
+expect \"Reload privilege tables now?\"
+send \"y\r\"
 ")
 
 echo "$SECURE_MYSQL"
