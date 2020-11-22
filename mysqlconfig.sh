577b936f2130fe9cc3fd6d6c8f528a2cd506782e
ZBejavu <67857471+ZBejavu@users.noreply.github.com>
2020-11-22 09:26:12 +0200

    test2

--- mysqlconfig.sh
+++ mysqlconfig.sh
@@ -3,8 +3,8 @@ file="mysqlconfig.txt"
 echo "use mysql;" > $file
 # echo "update user set plugin="mysql_native_password" where User='root';" >> $file
 # echo "UPDATE mysql.user SET authentication_string =PASSWORD('$1') WHERE User='root';" >> $file
-echo "CREATE USER 'zach'@'localhost' IDENTIFIED BY '$1';" >> $file
-echo "GRANT ALL PRIVILEGES ON *.* TO 'zach'@'localhost' WITH GRANT OPTION;" >> $file
+echo "CREATE USER '$2'@'localhost' IDENTIFIED BY '$1';" >> $file
+echo "GRANT ALL PRIVILEGES ON *.* TO '$2'@'localhost' WITH GRANT OPTION;" >> $file
 echo "flush privileges;" >> $file
 echo "exit;" >> $file
 cat $file
