43e293f1777740ea824d0f8568f50cdda13fe96f
ZBejavu <67857471+ZBejavu@users.noreply.github.com>
2020-12-22 22:29:22 +0200

    update

--- server/config/config.js
+++ server/config/config.js
@@ -2,14 +2,14 @@ require('dotenv').config();
 module.exports = {
 "development": {
     "username": process.env.MYSQL_USER || 'root',
-    "password": process.env.MYSQL_PASSWORD || 'password',
+    "password": process.env.MYSQL_PASSWORD || null,
     "database": process.env.MYSQL_DATABASE,
     "host": process.env.MYSQL_HOST, //process.env.DB_HOST || mysql
     "dialect": "mysql"
 },
 "test": {
     "username": process.env.MYSQL_USER || 'root',
-    "password": process.env.MYSQL_PASSWORD || 'password',
+    "password": process.env.MYSQL_PASSWORD || null,
     "database": process.env.MYSQL_DATABASE,
     "host": process.env.MYSQL_HOST || '127.0.0.1',
     "dialect": "mysql"
